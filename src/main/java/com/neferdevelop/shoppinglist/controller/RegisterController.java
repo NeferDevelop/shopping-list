6e7dfff71d060906a19f2d77b372ba1f151fedde
NeferDevelop <sergei.klimov814@gmail.com>
2024-01-09 22:21:09 +0300

    refactor: added a controller for better readability

---
+++ src/main/java/com/neferdevelop/shoppinglist/controller/RegisterController.java
@@ -0,0 +1,46 @@
+package com.neferdevelop.shoppinglist.controller;
+
+import com.neferdevelop.shoppinglist.service.UserReprisentation;
+import com.neferdevelop.shoppinglist.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class RegisterController {
+    private UserService userService;
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired // Чтобы Spring понял, что сюда нужно внедрить userService
+    public RegisterController(UserService userService) {
+        this.userService = userService;
+    }
+
+    // Отображает форму для ввода информации
+    @GetMapping("/register")
+    public String registerPage(Model model){
+        model.addAttribute("user", new UserReprisentation()); // добавляем пользователя в виде атрибута, внутри создаём класс
+        return "register";
+    }
+
+    // Метод, который обрабатывает эту самую информацию
+    @PostMapping("/register")
+    public String registerNewUser(@Valid UserReprisentation userReprisentation, BindingResult bindingResult){ // Последний параметр валидация (будет возвращаться информация о том, как была провалидирована информация из полей в userRepresentation на основании полей из userRepresentation)
+        if(bindingResult.hasErrors()){ // проверка, всё ли было хорошо при валидации
+            return "register";
+        }
+        if(!userReprisentation.getPassword().equals(userReprisentation.getRepeatPassword())){ // Проверяем равенство паролей
+            bindingResult.rejectValue("password","", "Пароли не совпадают");
+            return "register";
+        }
+
+        // После успешной валдиации создаём пользователя и возвращаемся на строницу login
+        userService.create(userReprisentation);
+        return "redirect:/login";
+    }
+}
